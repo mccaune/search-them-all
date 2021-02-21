e9edab2b0c146b3571a3f096d2543a93647ef332
mc <mairis.caune@gmail.com>
2021-02-21 12:09:09 +0200

    deploying project to github pages

---
+++ build/precache-manifest.7103e865f63e38c349338e3ef277ef92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c2e2fb097a0a08ba3d61",
+    "url": "/search-them-all/static/css/main.c86d6cc7.chunk.css"
+  },
+  {
+    "revision": "c2e2fb097a0a08ba3d61",
+    "url": "/search-them-all/static/js/main.7db2688b.chunk.js"
+  },
+  {
+    "revision": "d709a2b54c6c07ae8031",
+    "url": "/search-them-all/static/js/runtime~main.cfe9b9f8.js"
+  },
+  {
+    "revision": "03ae8d492bbfd271c7d9",
+    "url": "/search-them-all/static/css/2.1cd7ca8d.chunk.css"
+  },
+  {
+    "revision": "03ae8d492bbfd271c7d9",
+    "url": "/search-them-all/static/js/2.b9377898.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/search-them-all/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "4fcb644fa1b2868ecb891ae305fb3c82",
+    "url": "/search-them-all/index.html"
+  }
+];
